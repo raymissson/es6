e7f728e322b0dc0a39a19f20a3e9bb880054477e
Raymison Maklouf <31172150+raymissson@users.noreply.github.com>
2019-04-17 16:38:00 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+class List{
+    constructor(){
+        this.data =[];
+    }
+
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+class TodoList extends List{
+    constructor(){
+        super();
+        this.usuario = 'Raymison';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo Todo');
+}
