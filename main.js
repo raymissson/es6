54fb3afcfd26fc4426ca8fadfbbb2a641b29aa2c
Raymison Maklouf <31172150+raymissson@users.noreply.github.com>
2019-04-18 16:13:46 -0400

    template literals

--- main.js
+++ main.js
@@ -1,50 +1,7 @@
-// operadores rest - resto das propriedades
+// template literals
+const nome = 'Diego';
+const idade = 23;
 
-const usuario={
-    nome: 'Raymison',
-    idade: 23,
-    empresa: 'bshare'
-};
+//console.log('Meu nome é:'+nome+' e tenho: '+idade+' anos');
 
-const {nome, ...resto} = usuario;
-
-console.log(nome);
-console.log(resto);
-
-const arr = [1,2,3,4];
-const [a, b, ...c] = arr;
-
-console.log(a,b,c);
-// somar numeros sem definir variáveis para receber os números
-function soma(...params){
-    return params.reduce((total, next)=>total+next);
-}
-
-console.log(soma(1,2,3,4));
-// rest 
-
-function somarest(a,b,...params){
-    return params;
-}
-
-console.log(somarest(1,2,3,4));
-
-// spread
-const arr1 = [1,2,3];
-const arr2 = [4,5,6];
-
-const arr3 =[...arr1,...arr2];
-
-console.log(arr3);
-
-// spread
-
-const usu ={
-    nome: 'Raymison',
-    idade: 23,
-    empresa: 'bshare'
-};
-
-const usu2 = {...usu, nome: 'Maklouf'};
-
-console.log(usu2);
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
