da6d191f6293a201a64029b0517e6f5d59e5bf23
Raymison Maklouf <31172150+raymissson@users.noreply.github.com>
2019-04-18 16:07:46 -0400

    rest/spread

--- main.js
+++ main.js
@@ -1,19 +1,50 @@
-//desdestruturação
-const usuario = {
-    nome: 'raymison',
+// operadores rest - resto das propriedades
+
+const usuario={
+    nome: 'Raymison',
     idade: 23,
-    end: {
-        cidade: 'Manaus',
-        estado: 'AM'
-    }
+    empresa: 'bshare'
 };
 
-const {nome, idade, end:{cidade} } = usuario;
+const {nome, ...resto} = usuario;
 
-console.log(nome,idade,cidade);
+console.log(nome);
+console.log(resto);
 
-function mostraNome({nome, idade}){
-    console.log(nome,idade);
+const arr = [1,2,3,4];
+const [a, b, ...c] = arr;
+
+console.log(a,b,c);
+// somar numeros sem definir variáveis para receber os números
+function soma(...params){
+    return params.reduce((total, next)=>total+next);
 }
 
-mostraNome(usuario); 
+console.log(soma(1,2,3,4));
+// rest 
+
+function somarest(a,b,...params){
+    return params;
+}
+
+console.log(somarest(1,2,3,4));
+
+// spread
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+const arr3 =[...arr1,...arr2];
+
+console.log(arr3);
+
+// spread
+
+const usu ={
+    nome: 'Raymison',
+    idade: 23,
+    empresa: 'bshare'
+};
+
+const usu2 = {...usu, nome: 'Maklouf'};
+
+console.log(usu2);
